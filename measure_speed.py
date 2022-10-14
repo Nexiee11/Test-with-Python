c9480ed6737da42f0054fa3b727beb193195e5b1
Fedor Fatekhov <fedorfatekhov@MacBook-Pro-Fedor.local>
2022-10-15 00:25:04 +0300

    added files for speed test

--- measure_speed.py
+++ measure_speed.py
@@ -2,10 +2,11 @@ import timeit
 import random
 import matplotlib.pyplot as plt
 import f12
-import numpy
-arr = [random.sample(range(-1_000,1_000),k) for k in [1,10,20,30]]
+import numpy 
+test_arr = [1,10,20,30,50,100]
+arr = [random.sample(range(-1_000,1_000),k) for k in test_arr]
 
-time_res = {x: ''  for x in [1,10,20,30]}
+time_res = {x: ''  for x in test_arr}
 for x in arr:
     time_res[len(x)] = timeit.timeit(lambda:f12.all_func(x))
 
